b23c359a880f5583398d96a8142eca1382cb9433
Angelo Leva <angelokta7@gmail.com>
2020-07-30 16:07:45 -0500

    Updates

---
+++ precache-manifest.7862584817fdc2d1f7f09ed545a52d8a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9cb36df5610aec896940c4a06a79fc",
+    "url": "https://angelolev.github.io/cl-react/index.html"
+  },
+  {
+    "revision": "f6df45d5025c65c4df05",
+    "url": "https://angelolev.github.io/cl-react/static/css/main.89277576.chunk.css"
+  },
+  {
+    "revision": "4d50ce603615f0df1d9d",
+    "url": "https://angelolev.github.io/cl-react/static/js/2.21381c98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://angelolev.github.io/cl-react/static/js/2.21381c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6df45d5025c65c4df05",
+    "url": "https://angelolev.github.io/cl-react/static/js/main.f53f9da4.chunk.js"
+  },
+  {
+    "revision": "8f942061cd6b951d5292",
+    "url": "https://angelolev.github.io/cl-react/static/js/runtime-main.c680e5a2.js"
+  },
+  {
+    "revision": "6f48532129c407b746c3c698c61b0203",
+    "url": "https://angelolev.github.io/cl-react/static/media/icomoon.6f485321.eot"
+  },
+  {
+    "revision": "9a15aa0570c98f38d05406fdcc9bf502",
+    "url": "https://angelolev.github.io/cl-react/static/media/icomoon.9a15aa05.ttf"
+  },
+  {
+    "revision": "9c6ff23718e4fcc07858623597c89e38",
+    "url": "https://angelolev.github.io/cl-react/static/media/icomoon.9c6ff237.woff"
+  },
+  {
+    "revision": "9eb5b32fdc0419ed79b201769b85856e",
+    "url": "https://angelolev.github.io/cl-react/static/media/icomoon.9eb5b32f.svg"
+  },
+  {
+    "revision": "7216f74ffb01455387789ae34d29f592",
+    "url": "https://angelolev.github.io/cl-react/static/media/wave.7216f74f.svg"
+  },
+  {
+    "revision": "32a088df508c5759f2306ca347ac2b0f",
+    "url": "https://angelolev.github.io/cl-react/static/media/wave2.32a088df.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cl-react/precache-manifest.848cadfdf25b6f82984ea97424659c56.js"
+  "https://angelolev.github.io/cl-react/precache-manifest.7862584817fdc2d1f7f09ed545a52d8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cl-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://angelolev.github.io/cl-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
