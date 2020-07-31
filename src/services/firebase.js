214951795e96cccf5e01f24a51a55227f5f676c2
Jeyson Fuentes Alfaro <jfuentes@dcp.pe>
2020-07-31 00:15:54 -0500

    add firebase dependency  - update config firebase firestore  - update components lastGroups, lastCourses, lastTalks

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,11 @@
 import firebase from "firebase";
-import { config } from "../../firebaseConfig";
+import { config } from "./firebaseConfig";
+// import * as firebase from "firebase/app";
+// import "firebase/firestore";
+// import "firebase/auth";
 
 firebase.initializeApp(config);
 firebase.analytics();
 
 export const auth = firebase.auth();
-export const db = firebase.database();
+export const db = firebase.firestore();
