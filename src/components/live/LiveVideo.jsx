898cd6b746067edc6a7ad651f8a3e36900243792
Angelo Leva <angelokta7@gmail.com>
2020-07-30 22:49:38 -0500

    fixed Live page

--- src/components/live/LiveVideo.jsx
+++ src/components/live/LiveVideo.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const LiveVideo = () => {
   return (
     <section className="live__video">
-      <iframe
+      {/* <iframe
         width="100%"
         height="315"
         src="https://www.youtube.com/embed/6feo8GymG_Q"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
-      ></iframe>
+      ></iframe> */}
       <h3>Crea tu Github Profile con README</h3>
       <p>
         GitHub ha lanzado una nueva funcionalidad que te permite mostrar un
