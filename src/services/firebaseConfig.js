bbf6d4d59350e0ee7ee37120e07083717fb84c68
Angelo Leva <angelokta7@gmail.com>
2020-08-20 19:40:46 -0500

    added firebase config

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,10 @@
+export var config = {
+  apiKey: "AIzaSyBwkQvr28jbGKVNgIkegq3qvSIk5lny620",
+  authDomain: "coding-latam.firebaseapp.com",
+  databaseURL: "https://coding-latam.firebaseio.com",
+  projectId: "coding-latam",
+  storageBucket: "coding-latam.appspot.com",
+  messagingSenderId: "422443921553",
+  appId: "1:422443921553:web:494d758dfbc7bf822a8f30",
+  measurementId: "G-BY52FJ4TCK",
+};
